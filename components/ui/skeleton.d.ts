a916fc401aae712c6f34ba1304968c62b12f6243
Seon-U <seonu.kim.kr@gmail.com>
2025-08-29 19:04:46 +0900

    add some layout

---
+++ components/ui/skeleton.d.ts
@@ -0,0 +1,2 @@
+declare function Skeleton({ className, ...props }: React.ComponentProps<"div">): import("next/dist/server/route-modules/app-page/vendored/rsc/entrypoints").React.JSX.Element;
+export { Skeleton };
---
+++ components/ui/sonner.d.ts
@@ -0,0 +1,3 @@
+import { ToasterProps } from "sonner";
+declare const Toaster: ({ ...props }: ToasterProps) => import("next/dist/server/route-modules/app-page/vendored/rsc/entrypoints").React.JSX.Element;
+export { Toaster };
