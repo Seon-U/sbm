ebacea75d22abf04c1295c05846ade2134a23ad3
Seon-U <seonu.kim.kr@gmail.com>
2025-09-01 18:08:28 +0900

    add ORM & change CSS

--- components/theme-changer.tsx
+++ components/theme-changer.tsx
@@ -1,37 +1,34 @@
 'use client';
 
-import { MonitorIcon, MoonStar, StarIcon } from 'lucide-react';
+import { MonitorIcon, MoonStar, SunIcon } from 'lucide-react';
 import { useTheme } from 'next-themes';
-import { useEffect, useReducer, useState } from 'react';
+import { useEffect, useState } from 'react';
 
 //readonly처리를 위
 const THEMES = ['light', 'system', 'dark'] as const;
 const THEME_ICONS = {
   light: <MonitorIcon />,
   system: <MoonStar />,
-  dark: <StarIcon />,
+  dark: <SunIcon />,
 };
 
 export default function ThemeChanger() {
-  const { theme } = useTheme();
-  const [isDarkMode, setTheme] = useReducer(pre => !pre, false);
+  const { theme, setTheme } = useTheme();
   const [mount, setMount] = useState(false);
 
   useEffect(() => {
-    (setMount(true), []);
-  });
-
-  //나중에 off a11y해주면 됨
-  if (!mount) return <button></button>;
-
+    setMount(true);
+    return () => {};
+  }, []);
+  if (!mount || !theme) return <button></button>;
   const changeTheme = () => {
     const idx = THEMES.indexOf(theme as keyof typeof THEME_ICONS) + 1;
-    setTheme(THEMES[idx === THEMES.length ? 0 : idx]);
-
-    return (
-      <button type='button' onClick={changeTheme} className='btn-icon'>
-        {THEME_ICONS[theme as keyof typeof THEME_ICONS]} {theme}
-      </button>
-    );
+    setTheme(THEMES[idx % THEMES.length]);
   };
+
+  return (
+    <button onClick={changeTheme} className='btn-icon'>
+      {THEME_ICONS[theme as keyof typeof THEME_ICONS]}
+    </button>
+  );
 }
